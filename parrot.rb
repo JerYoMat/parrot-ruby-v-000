9f91566a7cc1f3d62a3d9949f35438ad65fef6e6
Jeremy Emata <jeremy.emata@gmail.com>
2018-09-28 06:43:47 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-  puts "Pretty bird!"
-  "Pretty bird!"
+def parrot(noise = "Squawk!")
+  puts noise
+  noise 
 end 
